3bbf761fe53bc7694bb29e4b2fe6450131265492
Marek Walica <mwalica@gmail.com>
2024-09-22 09:15:11 +0200

    init commit

---
+++ app/src/main/java/ch/walica/temp_fragment_args/ResultFragment.java
@@ -0,0 +1,28 @@
+package ch.walica.temp_fragment_args;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ResultFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
